d0e231a134e8ee096c78da78ab054706449f1f0c
judsonpaiva <judson.paiva@hotmail.com>
2022-03-16 01:10:33 +0100

    Atribuíndo evento no botão

--- app/src/main/java/com/example/appsorteio/MainActivity.java
+++ app/src/main/java/com/example/appsorteio/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.appsorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sortearValor(View view){
+
+        texto = findViewById(R.id.txt_resultado);
+        texto.setText("5");
+
+    }
+
 }
